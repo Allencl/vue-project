311d5c51f058268bc2e53f63a34a361113da99f9
Allencl <Allen__cc@163.com>
2020-06-07 14:37:09 +0800

    1

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,12 @@
 import VueRouter from 'vue-router';
 
 import RegisterPage from './components/Register.vue';
-
+import HomePage from './home.vue';
 
 
 const routes = [
-  { path: '/', component: RegisterPage },   // 注册页面
+  // { path: '/', component: RegisterPage },   // 注册页面
+  { path: '/', component: HomePage },   // home 页面
 ]
 
 export const router = new VueRouter({
