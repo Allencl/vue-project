cd9873a1616ea3947f4f90f962d4dc9d2a00b2d1
Allencl <Allen__cc@163.com>
2020-06-07 14:41:26 +0800

    1

--- src/home.vue
+++ src/home.vue
@@ -8,60 +8,7 @@
     export default {
         data () {
             return {
-                list:[
-                    {
-                        "label": "一级标题",
-                        "key": "77B1",
-                        "children": [
-                        {"label": "Alabama", "key": "AL"},
-                        {"label": "Alaska", "key": "AK"},
-                        ]
-                    },
-                    {
-                        "label": "第一级-多层的",
-                        "children": [
-                        {
-                            "label": "第二级", 
-                            "key": "B1",
-                            "children": [
-                            {
-                                "label": "第三级111", 
-                                "key": "B21",
-
-                                "children": [
-                                {
-                                    "label": "第四级111", 
-                                    "key": "rrruuAL",
-                                    "children": [
-                                    {
-                                        "label": "第五级111", 
-                                        "key": "wwwuuAL",
-                                        "children": [
-                                        {
-                                            "label": "第六级111", 
-                                            "key": "loouuAL",
-                                            "children": [
-                                            {
-                                                "label": "第七级111", 
-                                                "key": "mmjjuuAL"
-                                            }
-                                            ]                                 
-                                        }
-                                        ]                            
-                                    }
-                                    ]
-                                }
-                                
-                                ]
-
-                            },
-                            {"label": "第三级222", "key": "B22"},
-                            ]
-                        },
-                        {"label": "Alaska", "key": "B2"},
-                        ]
-                    },  
-                ]                
+               
             }
         },
 		created() {
@@ -70,55 +17,113 @@
         methods: {
             click: function(){
 
-            var res = [{
-                key: "a1111",
-                label: "湖北省",
-                children: [{
-                    key: "aa111",
-                    label: "武汉市",
-                    children: [{
-                        key: "ab11",
-                        label: "汉阳区",
-                        children: [{
-                            key: "abgt11",
-                            label: "水上分局1"
-                        }]
-                    }, 
-                    {
-                        key: "auitut8",
-                        label: "武昌区",
-                        children: [{
-                            key: "ajuyt66",
-                            label: "水上分局2"
-                        }]
-                    }, {
-                        key: "akiju8876",
-                        label: "汉口区",
-                        children: [{
-                            key: "aoise7",
-                            label: "水上分局3"
-                        }]
-                    }]
-                }, {
-                    key: "c3333",
-                    label: "十堰市",
-                    children: [{
-                        key: "ku877eee",
-                        label: "郧阳区",
-                        children: [{
-                            key: "kisu83yh",
-                            label: "安阳镇"
-                        }]
-                    }, {
-                        key: "jiuuee",
-                        label: "茅箭区",
-                        children: [{
-                            key: "kisuejjs655",
-                            label: "小川乡"
-                        }]
-                    }]
-                }]
-            }];
+            // var res = [{
+            //     key: "a1111",
+            //     label: "湖北省",
+            //     children: [{
+            //         key: "aa111",
+            //         label: "武汉市",
+            //         children: [{
+            //             key: "ab11",
+            //             label: "汉阳区",
+            //             children: [{
+            //                 key: "abgt11",
+            //                 label: "水上分局1"
+            //             }]
+            //         }, 
+            //         {
+            //             key: "auitut8",
+            //             label: "武昌区",
+            //             children: [{
+            //                 key: "ajuyt66",
+            //                 label: "水上分局2"
+            //             }]
+            //         }, {
+            //             key: "akiju8876",
+            //             label: "汉口区",
+            //             children: [{
+            //                 key: "aoise7",
+            //                 label: "水上分局3"
+            //             }]
+            //         }]
+            //     }, {
+            //         key: "c3333",
+            //         label: "十堰市",
+            //         children: [{
+            //             key: "ku877eee",
+            //             label: "郧阳区",
+            //             children: [{
+            //                 key: "kisu83yh",
+            //                 label: "安阳镇"
+            //             }]
+            //         }, {
+            //             key: "jiuuee",
+            //             label: "茅箭区",
+            //             children: [{
+            //                 key: "kisuejjs655",
+            //                 label: "小川乡"
+            //             }]
+            //         }]
+            //     }]
+            // }];
+
+
+        var res = [
+          {
+            "label": "一级标题",
+            "key": "77B1",
+            "children": [
+              {"label": "Alabama", "key": "AL"},
+              {"label": "Alaska", "key": "AK"},
+            ]
+          },
+          {
+            "label": "第一级-多层的",
+            "key": "ttt77B1",
+            "children": [
+              {
+                "label": "第二级", 
+                "key": "B1",
+                "children": [
+                  {
+                    "label": "第三级111", 
+                    "key": "B21",
+
+                    "children": [
+                      {
+                        "label": "第四级111", 
+                        "key": "rrruuAL",
+                        "children": [
+                          {
+                            "label": "第五级111", 
+                            "key": "wwwuuAL",
+                            "children": [
+                              {
+                                "label": "第六级111", 
+                                "key": "loouuAL",
+                                "children": [
+                                  {
+                                    "label": "第七级111", 
+                                    "key": "mmjjuuAL"
+                                  }
+                                ]                                 
+                              }
+                            ]                            
+                          }
+                        ]
+                      }
+                      
+                    ]
+
+                  },
+                  {"label": "第三级222", "key": "B22"},
+                ]
+              },
+              {"label": "Alaska", "key": "B2"},
+            ]
+          },  
+        ]; 
+
 
             function treeFindPath (tree, func, path = []) {
                 if (!tree) return []
@@ -139,7 +144,7 @@
             }
 
 
-            var aaa = treeFindPath(res, data=> data.key==='kisu83yh');
+            var aaa = treeFindPath(res, data=> data.key==='mmjjuuAL');
 
             console.log(aaa);
             console.log(res);
