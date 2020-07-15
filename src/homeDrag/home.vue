d0b8f1b01e1864c9c6765394826d46482cdd5db3
Allencl <Allen__cc@163.com>
2020-07-15 15:36:12 +0800

    1

--- src/homeDrag/home.vue
+++ src/homeDrag/home.vue
@@ -1,45 +1,91 @@
 <style lang="scss">
 
   .custom-drag-home{
+    // position: relative;
+
+    &.gridding{
+      background-image: 
+        linear-gradient(#000 1px, transparent 0),
+        linear-gradient(90deg, #000 1px, transparent 0);
+      background-size: 20px 20px;
+    }
 
     .flip-list-move {
-      transition: transform 0.7s;
+      // transition: transform 0.7s;
     }
 
     .no-move {
-      transition: transform 0.7s;
+      // transition: transform 0.7s;
     }
 
     .ghost {
       opacity: 0.5;
       background: #c8ebfb;
     }
+
+
     
     .custom-home-list-group{
+
       >span{
         display: inline-block;
       }
 
       .custom-home-list-group-item{
-        min-width: 100px;
-        min-height: 100px;
+        position: relative;
+        // min-width: 100px;
+        // min-height: 100px;
         float: left;
         margin: 6px 6px;
         border: 1px solid #dcdee2;
         border-color: #e8eaec;
         border-radius: 4px;
         box-shadow: 2px 2px 4px 1px rgba(184, 184, 184, 0.4);
-        transition: all 0.5s;
+        background: #fff;
+        // transition: all 0.5s;
+
+        .controller-box{
+          display: none;
+          position: relative;
+
+          .right-bottom{
+            position: absolute;
+            right: -5px;
+            bottom: -5px;
+            display: block;
+            width: 12px;
+            height: 12px;
+            background: #fff;
+            border: 1px solid #ccc;
+            z-index: 111;
+            cursor: nw-resize;
+          }
+        }
 
 
         &.is-checked{
-          border-width: 2px;
+          border-width: 1.5px;
           border-style: dashed;
           box-shadow: none;
           border-color: rgba(100,149,237,0.8);
+
+          .controller-box{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            z-index: 11;
+            display: block;
+            width: 100%;
+            height: 100%;
+            background: yellow;
+          }
         }
 
         .move-handle{
+          position: absolute;
+          top: 0px;
+          left: 0px;
+          z-index: 111;
           line-height: 30px;
           text-align: center;
           cursor: all-scroll;
@@ -57,7 +103,7 @@
 
 </style>
 <template>
-  <div class="custom-drag-home">
+  <div class="custom-drag-home gridding">
     <!-- <div>
       <button class="btn btn-secondary button" @click="resetHandle">重置</button>
     </div> -->
@@ -65,7 +111,6 @@
     <div v-if="config['chidden']['length']">
       <draggable
         class="custom-home-list-group"
-        
         v-model="list"
         v-bind="dragOptions"
         handle=".handle"
@@ -74,7 +119,6 @@
       >
         <!-- <transition-group type="transition" :name="!drag ? 'flip-list' : null"> -->
         <transition-group type="transition">
-
           <div
             v-for="(o,i) in list"
             :key="i"
@@ -83,8 +127,13 @@
             v-on:click.self="groupItemHandle(o,$event)"
           >
             <div class="handle move-handle"><Icon type="md-move" /></div>
+            <div class="controller-box">
+              <b v-drag_arrows="{data:o}" class="right-bottom" :title="`${o['width']} x ${o['height']}`"></b>
+            </div>
             <!-- <div><Icon type="md-move" /></div> -->
-            
+            <div>
+              <h1>ninini</h1>
+            </div>
           </div>
         </transition-group>
       </draggable>
@@ -114,8 +163,8 @@ export default {
   data(_this) {
     return {
       drag: false,
-      keyChecked: undefined,   // 选中的 盒子
-      eventTarget: undefined,  // 目标 event
+      keyChecked: undefined,      // 选中的 盒子
+      eventTarget: undefined,     // 目标 event
 
       // list 数据
       list:(_this.config['chidden']||[]).map((o,i)=> Object.assign({},o,{_key:i})),
@@ -159,8 +208,28 @@ export default {
       // console.log(this.list);
       this.list = this.list.sort((a, b) => a._key - b._key);
     }
+  },
+  directives:{
+    drag_arrows(el,option){
+      el.onmousedown = function(e){
+        var pNode=e.target.parentNode.parentNode;
+        var disX=e.pageX;
+        var disY=e.pageY;
+        var disW=option.value.data["width"];
+        var disH=option.value.data["height"];
+
+        document.onmousemove = function (e){
+          option.value.data["width"]=disW+(e.pageX-disX);
+          option.value.data["height"]=disH+(e.pageY-disY);
+        }
+
+        document.onmouseup = function(){
+          document.onmousemove = document.onmouseup = null;
+        }
+      }
+    }
   }
-};
+}
 </script>
 
 
