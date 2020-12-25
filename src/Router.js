850d8d57147e1155689b0995fb65579f8bb97785
Allencl <Allen__cc@163.com>
2020-12-25 16:09:06 +0800

    1

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,15 @@
 import VueRouter from 'vue-router';
 
 // import HomePage from './homeDrag/index.vue';   // 首页
-// import RegisterPage from './components/Register.vue';
-import LargeScreenPage from './largeScreen/index.vue';   // 大屏
+import RegisterPage from './components/Register.vue';
+// import LargeScreenPage from './largeScreen/index.vue';   // 大屏
 // import LargeScreenPage2 from './home2/index.vue';   // 大屏
 // import TinymcePage from './tinymce/index.vue';   // 富文本 编辑器
 
 
 const routes = [
   // { path: '/', component: RegisterPage },   // 注册页面
-  { path: '/', component: LargeScreenPage },   // home 页面
+  { path: '/', component: RegisterPage },   // home 页面
 
 ]
 
