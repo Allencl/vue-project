4a00201d34f4a86b399768d3f7caef58cc1ca6d8
Allencl <Allen__cc@163.com>
2020-12-30 15:00:56 +0800

    1

--- src/Router.js
+++ src/Router.js
@@ -6,10 +6,13 @@ import RegisterPage from './components/Register.vue';
 // import LargeScreenPage2 from './home2/index.vue';   // 大屏
 // import TinymcePage from './tinymce/index.vue';   // 富文本 编辑器
 
+import TablePage from './table/index.vue';
+
+
 
 const routes = [
   // { path: '/', component: RegisterPage },   // 注册页面
-  { path: '/', component: RegisterPage },   // home 页面
+  { path: '/', component: TablePage },   // home 页面
 
 ]
 
---
+++ src/table/index.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <div>
+            <button @click="getData">测试按钮</button>
+            <button @click="saveData">保存数据按钮</button>
+
+        </div>
+        <div>
+            <Table :columns="columns" :data="data"></Table>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                // 列配置 信息
+                columns: [],
+
+                // 数据
+                data: []
+            }
+        },
+        methods: {
+            /**
+             * 保存数据
+             */
+            saveData: function(){
+                console.log( this.data );
+            },
+            /**
+             *  初始化
+             */
+            getData: function(){
+                // 模拟 请求数据 替换 
+                let that=this;
+
+                setTimeout(()=>{
+
+                    // 列数据
+                    let _columns=["2021-01-01","2021-01-02","2021-01-03"];  
+                    // 行数据
+                    let _data=[
+                        {
+                            "equipment":"设备一",
+                            "2021-01-01":"111",
+                            "2021-01-02":"22",
+                            "2021-01-03":"33"
+                        },
+                        {
+                            "equipment":"设备二"
+                        },
+                        {
+                            "equipment":"设备三"
+                        }                                        
+                    ];
+
+
+                    that.columns= [
+                        {
+                            title: "设备",
+                            key: "equipment",
+                            width:100,
+                        }
+                    ].concat(_columns.map((o,index)=>{
+                        return({
+                            title: o,
+                            key: o,
+                            width:300,
+                            render: (h, params) => {
+                                return h('div', [
+                                    h('Input', {
+                                        props: {
+                                            value:params.row[o],
+                                            placeholder:"请输入..."
+                                        },
+                                        on: {
+                                            input: event => {
+                                                that.data[params.index][o]= event; //双向绑定 修改值
+                                            }
+                                        }
+                                    }),
+                                ]);
+                            }
+                        });
+                    }));
+
+                    that.$nextTick(()=>{
+                        that.data=_data;
+                    });
+                },1000);
+            }
+        },        
+    }
+</script>
