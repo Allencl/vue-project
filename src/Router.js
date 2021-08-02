5bf8e201819d719795a82f1951382c0a6af5c166
Allencl <Allen__cc@163.com>
2021-08-02 11:23:06 +0800

    1

--- src/Router.js
+++ src/Router.js
@@ -9,19 +9,22 @@ import LargeScreenPage2 from './home3/index.vue';   // 大屏
 
 
 // import Page from './board/index1.vue';
-import Board1 from './board/index1.vue';
-import Board2 from './board/index2.vue';
+// import Board1 from './board/index1.vue';
+// import Board2 from './board/index2.vue';
 
 
 // import MessagePage from './message/index.vue';
 // D:\code\vue-project\src\board\index1.vue
 
+import TableEditPage from './table/table-edit.vue';   // 可编辑 table
+
+
+
 const routes = [
   // { path: '/', component: RegisterPage },   // 注册页面
-  { path: '/', component: LargeScreenPage2 },   // home 页面
+  { path: '/', component: TableEditPage },   // home 页面
 
-  { path: '/board/index1', component: Board1 },   
-  { path: '/board/index2', component: Board2 },   
+
 
 
 
---
+++ src/table/table-edit.vue
@@ -0,0 +1,61 @@
+<template>
+    <span>
+        <Button type="info" @click="getData">获取数据</Button>
+        <Table border :columns="columns" :data="data">
+            <template slot-scope="{ row }" slot="name">
+                <strong>{{ row.name }}</strong>
+            </template>
+            <template slot-scope="{row,index}" slot="input">
+                <span>{{index}}</span>
+                <Input v-model="row.name" placeholder="Enter something..." style="width: 300px" />
+            </template>        
+            <template slot-scope="{ row, index }" slot="action">
+                <Button type="primary" size="small" style="margin-right: 5px" @click="show(index)">View</Button>
+                <Button type="error" size="small" @click="remove(index)">Delete</Button>
+            </template>
+        </Table>
+    </span>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                columns: [
+                    {
+                        title: 'Name',
+                        slot: 'name'
+                    },
+                    {
+                        title: '输入框',
+                        slot: 'input'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    },
+                    {
+                        title: 'Action',
+                        slot: 'action',
+                        width: 150,
+                        align: 'center'
+                    }
+                ],
+                data: [
+                    {
+                        name: 'John Brown',
+                        input: 'New York No'
+                    },
+                    {
+                        name: 'John Brown',
+                        input: '1 Lake Park'
+                    },
+                ]
+            }
+        },
+        methods: {
+            getData:function(){
+                console.log(this.data[0]);
+            }
+        }
+    }
+</script>
